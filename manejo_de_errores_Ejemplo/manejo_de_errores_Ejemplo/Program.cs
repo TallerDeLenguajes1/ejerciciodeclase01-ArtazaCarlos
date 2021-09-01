fcc148aa90aaa66f04987648c300160d570718c1
Artaza_Carlos <carlos.unt08@gmail.com>
2021-08-31 22:15:56 -0300

    agrego carpeta con el ejercicio de ejemplo de try-catch 'busqueda del numero aleatorio'

---
+++ manejo_de_errores_Ejemplo/manejo_de_errores_Ejemplo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PrecticaTallerII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random objNum = new Random();
+            int num = objNum.Next(11);
+            int num2 = -1;
+            int intentos = 0;
+
+            Console.WriteLine("Se genera aleatoriamente un numero entre 1 y 10");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Trate de adivinar el numero, ingrese un valor entre 1 y 10");
+
+            while (num != num2)
+            {
+                try
+                {
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                    if (num > num2)
+                    {
+                        Console.WriteLine("El valor ingresado es menor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El valor ingresado es mayor");
+                    }
+                    Console.WriteLine("ingrese otro valor");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("El valor ingresado no es valido");
+                }
+                intentos++;
+            }
+
+            Console.WriteLine("**********************************");
+            Console.WriteLine("el numero es el correto: " + num + " en el intento nro: " + intentos);
+            Console.WriteLine("**********************************");
+        }
+    }
+}
